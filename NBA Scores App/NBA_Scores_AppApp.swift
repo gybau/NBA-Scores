0df25a83ec56f282d243ba50cbb4aaf16b86285d
Michał Ganiebny <michalganiebny@rocketmail.com>
2022-12-04 12:38:04 +0100

    Initial Commit

---
+++ NBA Scores App/NBA_Scores_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_Scores_AppApp.swift
+//  NBA Scores App
+//
+//  Created by Michał Ganiebny on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct NBA_Scores_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
